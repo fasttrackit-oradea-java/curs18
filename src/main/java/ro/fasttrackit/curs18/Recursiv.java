e891660644bb88e8a93d639dcd44bf5a0ef0bc6c
Stefan Bulzan <Stefan.Bulzan@cloudflight.io>
2020-04-06 19:00:48 +0300

    test empty string : RED

--- src/main/java/ro/fasttrackit/curs18/Recursiv.java
+++ src/main/java/ro/fasttrackit/curs18/Recursiv.java
@@ -3,6 +3,6 @@ package ro.fasttrackit.curs18;
 public class Recursiv {
     public int length(String word) {
         if (word == null) throw new IllegalArgumentException("word cannot be null");
-        return 0;
+        return 1;
     }
 }
--- src/test/java/ro/fasttrackit/curs18/RecursivTest.java
+++ src/test/java/ro/fasttrackit/curs18/RecursivTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RecursivTest {
@@ -19,4 +20,13 @@ public class RecursivTest {
     void ifNullThenException() {
         assertThrows(IllegalArgumentException.class, () -> recursiv.length(null));
     }
+
+    @Test
+    @DisplayName("WHEN empty string is received THEN 0 is returned")
+    void emptyString() {
+        assertThat(recursiv.length("")).isEqualTo(0);
+    }
 }
+
+// empty string return 0 : RED
+// empty string return 0 : GREEN
