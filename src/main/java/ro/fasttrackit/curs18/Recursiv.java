d3b7675ef6b7e7f996d7353477c7cb22aafd8982
Stefan Bulzan <Stefan.Bulzan@cloudflight.io>
2020-04-06 18:52:02 +0300

    tested null input

--- src/main/java/ro/fasttrackit/curs18/Recursiv.java
+++ src/main/java/ro/fasttrackit/curs18/Recursiv.java
@@ -2,6 +2,7 @@ package ro.fasttrackit.curs18;
 
 public class Recursiv {
     public int length(String word) {
+        if (word == null) throw new IllegalArgumentException("word cannot be null");
         return 0;
     }
 }
